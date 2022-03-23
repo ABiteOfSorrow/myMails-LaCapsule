3b19cd73ed1b9dd4c14b0914e8dc4c83ad7b1659
ABiteOfSorrow <laceican@naver.com>
2022-03-23 16:02:50 +0100

    cleaning and change script(getId => getClass for counterNumber

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let messageCount = document.getElementsByClassName('listCont').length;
-document.getElementById('countNumber').textContent = messageCount;
-
-let countMessage = document.getElementById('countNumber').textContent;
+//getElementsByClassName은 배열 요소만 찾을 수 있기 때문에 [0]을 써줘야하고
+//getElementsById는 단일 요소만 찾을 수 있다.
+document.getElementsByClassName('countNumber')[0].textContent = messageCount;
+let countMessage = document.getElementsByClassName('countNumber').textContent;
 let trashLength = document.getElementsByClassName('imgPb').length;
 
 
@@ -11,7 +12,7 @@ for (let i = 0; i<trashLength; i++) {
     document.getElementsByClassName('imgPb')[i].addEventListener("click", function() {
         this.parentNode.remove();
         messageCount = document.getElementsByClassName('listCont').length;
-        document.getElementById('countNumber').textContent = messageCount;
+        document.getElementsByClassName('countNumber')[0].textContent = messageCount;
     }
     );
 }
