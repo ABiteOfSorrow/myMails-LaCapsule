07b0788af9c0fe01f69b74884e824e3d34c54a7f
ABiteOfSorrow <laceican@naver.com>
2022-03-27 19:33:40 +0200

    code structure change with jquery and add message find function

--- script.js
+++ script.js
@@ -1,72 +1,67 @@
 // Message counter function 
 
-let messageCount = document.getElementsByClassName('listCont').length;
+let messageCount = $('.listCont').length;
 //getElementsByClassName은 배열 요소만 찾을 수 있기 때문에 [0]을 써줘야하고
 //getElementsById는 단일 요소만 찾을 수 있다.
-document.getElementsByClassName('countNumber')[0].textContent = messageCount;
-let countMessage = document.getElementsByClassName('countNumber').textContent;
-let trashLength = document.getElementsByClassName('imgPb').length;
+$('#countNumber').text(messageCount);
+let countMessage = $('#countNumber').text();
+let trashLength = $('.imgPb').length;
+
 
 // Message delete function : trash button
-for (let i = 0; i < trashLength; i++) {
-    document.getElementsByClassName('imgPb')[i].addEventListener("click", function () {
-        this.parentNode.remove();
-        messageCount = document.getElementsByClassName('listCont').length;
-        document.getElementsByClassName('countNumber')[0].textContent = messageCount;
-    });
-}
+
+$('.imgPb').click(function () {
+    $(this).parent().remove();
+    messageCount = $('.listCont').length;
+    $('#countNumber').text(messageCount);
+});
 
 // Add message function : add button
-let addBtn = document.getElementById('addBtn');
-let mailListBody = document.getElementById('mailList');
-let mailContentBody = document.getElementsByClassName('listCont');
+
+let randomNumber = Math.floor(Math.random() * 5) + 1;
+let defaultName = ["Toldi Raldi", "Ali Bali", "Holy Moly", "Dolly Dilly", "Gully Golly", "Coin Cuin"];
 
 
+$('#addBtn').click(function () {
+    if ($('#newMessageTxt').val().length > 0) {
+        $('#mailList').append(`
+        <div class="listCont">
+                <img class="imgAvt" src="assets/avatar-${(Math.floor(Math.random() * 5) + 1)}.jpg">
+                <div>
+                    <h6>${defaultName[Math.floor(Math.random() * 5) + 1]}</h6>
+                    <P>${$('#newMessageTxt').val()}</P>
+                </div>
+                <img class="imgPb" src="assets/trash.png">
+        </div>`);
 
-addBtn.addEventListener("click", function () {
-    let createBody1 = document.createElement('div')
-    let createBody2 = document.createElement('div');
-    let createName = document.createElement('h6');
-    let createPharagraphe = document.createElement('p');
-    let createImg1 = document.createElement('img');
-    let createImg2 = document.createElement('img');
-    let randomNumber = Math.floor(Math.random() * 5) + 1;
-    let newMessageTxt = document.getElementById('newMessageTxt').value;
-    let defaultName = ["Toldi Raldi", "Ali Bali", "Holy Moly", "Dolly Dilly", "Gully Golly", "Coin Cuin"];
-
-
-    // Create message body, class = "listCont"
-    if (newMessageTxt.length > 0) {
-        mailListBody.appendChild(createBody1);
-        createBody1.className = "listCont";
-        // Add avartar img
-        createBody1.appendChild(createImg1);
-        createImg1.className = "imgAvt";
-        createImg1.setAttribute('src', '/assets/avatar-' + randomNumber + '.jpg');
-        // Add name
-        createBody1.appendChild(createBody2)
-        createBody1.lastElementChild.appendChild(createName);
-        createName.innerText = defaultName[randomNumber];
-        // Add message text
-        createBody1.lastElementChild.appendChild(createPharagraphe);
-        createPharagraphe.innerText = newMessageTxt;
-        createPharagraphe.className = "mailCont";
-        // Add trash image
-        createBody1.appendChild(createImg2);
-        createImg2.className = "imgPb";
-        createImg2.setAttribute('src', '/assets/trash.png');
         // Add delete and recount(after delete) function
-
-        createImg2.addEventListener("click", function () {
-            this.parentNode.remove();
-            messageCount = document.getElementsByClassName('listCont').length;
-            document.getElementsByClassName('countNumber')[0].textContent = messageCount;
+        $('.imgPb').click(function () {
+            $(this).parent().remove();
+            messageCount = $('.listCont').length;
+            $('#countNumber').text(messageCount);
         });
+
         // Return default value for input
-        document.getElementById('newMessageTxt').value = null;
+        $('#newMessageTxt').val(null);
 
         // Add counter after add new message
-        messageCount = document.getElementsByClassName('listCont').length;
-        document.getElementsByClassName('countNumber')[0].textContent = messageCount;
+        messageCount = $('.listCont').length;
+        $('#countNumber').text(messageCount);
     }
+})
+
+// Add message search function
+$('#searchBtn').click(function () {
+    if ($('#findMessage').val().length > 0) {
+        $('h6').each(function () {
+            if ($('#findMessage').val() != $(this).text()) {
+                $(this).parent().parent().fadeOut();
+            } else {
+                $(this).parent().parent().show();
+            } 
+        });
+
+        $('#findMessage').val(null);
+    }
+
 });
